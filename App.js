62fa88133d9e568dbd823df39868137e1ec776fe
Your Name <quytvgcd17298@fpt.edu.vn>
2021-08-16 21:08:55 +0700

    commit

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [counter, setCounter] = useState(0);
-  const [name, setName] = useState("Tran Van Quy");
+  const [name, setName] = useState("Name: TRAN VAN QUY");
   const decreaseCounter = ()=> {setCounter(counter - 1)};
   return (
-    <View style={styles.container}>
-      <Text style={styles.Text}>Hello Word!!!</Text>
+      <View style={styles.container}>
+     {/*  <Text style={styles.Text}>Hello Word!!!</Text>
 
       <Text 
       style={{ backgroundColor:'blue', fontSize: 20 }}
@@ -16,28 +16,63 @@ export default function App() {
         Counter: {counter}
       </Text>
 <Text>
-  {name}
+{name}
 </Text>
       <Button 
       onPress={()=> setCounter(counter + 1)} 
-      title="Increase" color="#003300"
+      title="Increase" 
+      color="#003300"
      />
       <Button 
       onPress={decreaseCounter} 
       title="Decrease" 
       color= "#3366FF" 
       />
-      <StatusBar style="auto" />
+      <StatusBar/> */}
+<View style = {styles.View}>
+      <Text>View 1</Text>
     </View>
+    <View style = {styles.View2}>
+      <Text>View 2</Text>
+    </View>
+    <View style = {styles.View3}>
+      <Text>View 3</Text>
+    </View>
+    </View>     
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  View: {
+    flex: 5,
+    height:100,
+    width: 100,
+    backgroundColor: "green",
+    alignItems:"center",
+    justifyContent:"center",
+  },
+   View2: {
+    flex: 2,
+    height:100,
+    width: 100,
+    backgroundColor: "blue",
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  View3: {
+    flex: 2,
+    height:100,
+    width: 100,
+    backgroundColor: "red",
+    alignItems:"center",
+    justifyContent:"center",
   },
   Text:{
     backgroundColor:"red",
