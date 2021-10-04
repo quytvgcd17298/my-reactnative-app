f4decd3d0770ac57dc22b01207b955a8ff89e137
Your Name <quytvgcd17298@fpt.edu.vn>
2021-10-04 23:31:44 +0700

    DrawerNavigation

--- App.js
+++ App.js
@@ -3,42 +3,26 @@ import { StyleSheet,
          View,
          Button, 
          TouchableOpacity} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator } from 'react-native-screens/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer'
 import React from 'react';
 import ScreenA from './screens/ScreenA';
 import ScreenB from './screens/ScreenB';
-import { Octicons } from '@expo/vector-icons'; 
 
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator ();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
    <NavigationContainer>
-     <Tab.Navigator 
-     screenOptions = {({route}) => ({
-       tabBarIcon:({focused}) => {
-       if(route.name === "ScreenA") 
-       {
-         return (
-          <Octicons name="person" size={24} color= {focused? "blue" : "black"} />
-         );
-       }
-       else if (route.name === "ScreenB")
-       {
-         return ( 
-         <Octicons name="three-bars" size={24} color={focused? "blue" : "black"} />
-         );
-       }
-     },
-    })}
-     >
-     <Tab.Screen name = "ScreenA" component = {ScreenA} options={{ tabBarBadge: 3 }}/>
-     <Tab.Screen name = "ScreenB" component = {ScreenB} options={{ tabBarBadge: 1 }}/>
-     </Tab.Navigator>
+     <Drawer.Navigator>
+     <Drawer.Screen name = "ScreenA" component = {ScreenA} options={{ tabBarBadge: 3 }}/>
+     <Drawer.Screen name = "ScreenB" component = {ScreenB} options={{ tabBarBadge: 1 }}/>
+     </Drawer.Navigator>
    </NavigationContainer>
   );
 }
