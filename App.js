f0e2a8fd7882ec17eb6edebabbba861daf7e86a4
Your Name <quytvgcd17298@fpt.edu.vn>
2021-10-05 00:13:15 +0700

    Option for Drawer-navigation

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator } from 'react-native-screens/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createDrawerNavigator} from '@react-navigation/drawer'
+import { Octicons } from '@expo/vector-icons'; 
 import React from 'react';
 import ScreenA from './screens/ScreenA';
 import ScreenB from './screens/ScreenB';
@@ -19,9 +20,27 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
    <NavigationContainer>
-     <Drawer.Navigator>
-     <Drawer.Screen name = "ScreenA" component = {ScreenA} options={{ tabBarBadge: 3 }}/>
-     <Drawer.Screen name = "ScreenB" component = {ScreenB} options={{ tabBarBadge: 1 }}/>
+     <Drawer.Navigator
+     initialRouteName = "ScreenB"
+     hireStartusBar = {true}
+     >
+     <Drawer.Screen 
+     name = "ScreenA"
+     component = {ScreenA} 
+     options= {{
+       title: " Screen A bar",
+       drawerIcon: () => (
+          <Octicons name="person" size={30} color= "black" /> ),
+     }}/>
+     <Drawer.Screen 
+     name = "ScreenB" 
+     component = {ScreenB} 
+     options= {{
+      title: " Screen B bar",
+      drawerIcon: () => (
+        <Octicons name="three-bars" size={30} color="black"  />
+      )
+      }}/>
      </Drawer.Navigator>
    </NavigationContainer>
   );
