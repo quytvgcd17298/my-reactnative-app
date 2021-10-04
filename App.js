50f4a5efd73ab30f7e5a24daaf54861d32a9657d
Your Name <quytvgcd17298@fpt.edu.vn>
2021-10-04 22:18:07 +0700

    Use Tab-navigation and vector icons

--- App.js
+++ App.js
@@ -5,20 +5,41 @@ import { StyleSheet,
          TouchableOpacity} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator } from 'react-native-screens/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
 import ScreenA from './screens/ScreenA';
 import ScreenB from './screens/ScreenB';
+import { AntDesign } from '@expo/vector-icons'; 
+import { Octicons } from '@expo/vector-icons'; 
 
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator ();
 
 export default function App() {
   return (
    <NavigationContainer>
-     <Stack.Navigator>
-     <Stack.Screen name = "ScreenA" component = {ScreenA}/>
-     <Stack.Screen name = "ScreenB" component = {ScreenB}/>
-     </Stack.Navigator>
+     <Tab.Navigator 
+     screenOptions = {({route}) => ({
+       tabBarIcon:({focused}) => {
+       if(route.name === "ScreenA") 
+       {
+         return (
+          <Octicons name="person" size={24} color= {focused? "blue" : "black"} />
+         );
+       }
+       else if (route.name === "ScreenB")
+       {
+         return ( 
+         <Octicons name="three-bars" size={24} color={focused? "blue" : "black"} />
+         );
+       }
+     },
+    })}
+     >
+     <Tab.Screen name = "ScreenA" component = {ScreenA}/>
+     <Tab.Screen name = "ScreenB" component = {ScreenB}/>
+     </Tab.Navigator>
    </NavigationContainer>
   );
 }
